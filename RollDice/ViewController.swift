4b02abbb04c9487be154afeed943ac88f184b6aa
Menghui Wang <wang.mengh@northeastern.edu>
2021-09-16 14:49:03 -0700

    Initial Commit

---
+++ RollDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RollDice
+//
+//  Created by Sally Wang on 2021/9/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
